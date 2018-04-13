05ef64df63d595159f1449ffbcd82898d81f8b04
Matheus Hahn <matheus.hahn@gmail.com>
2018-04-12 21:43:09 -0300

    identify eol

--- br/unisinos/tradutores/Test.java
+++ br/unisinos/tradutores/Test.java
@@ -8,7 +8,7 @@ import java.io.StringReader;
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		Reader r = new StringReader("/*abc*/if(x == 7 + 5) ;//oi");
+		Reader r = new StringReader("/*abc*/if(x == 7 + 5) \n abc ;//oi");
 		StreamTokenizer st = new StreamTokenizer(r);
 		st.slashSlashComments(true);
 		st.slashStarComments(true);
@@ -17,17 +17,17 @@ public class Test {
 		while (currentToken != StreamTokenizer.TT_EOF) {
 			switch (currentToken) {
 			case StreamTokenizer.TT_NUMBER:
-				System.out.printf("[num, %s]\n", st.nval);
+				System.out.printf("[num, %s]", st.nval);
 				break;
 			case StreamTokenizer.TT_WORD:
 				String word = st.sval;
-				System.out.printf("[string_literal, %s]\n", word);
+				System.out.printf("[string_literal, %s]", word);
 				break;
 			case '+':
 			case '-':
 			case '/':
 			case '*':
-				System.out.printf("[Arith_Op, %c]\n", currentToken);
+				System.out.printf("[Arith_Op, %c]", currentToken);
 				break;
 			case '<': {
 				int t = st.nextToken();
@@ -72,11 +72,14 @@ public class Test {
 				}
 			}
 			}
+			if(currentToken == StreamTokenizer.TT_EOL) {
+				System.out.println();
+			}
 			currentToken = st.nextToken();
 		}
 	}
 	
 	private static void printOperator(String operator) {
-		System.out.printf("[Relational_Op, %s]\n", operator);
+		System.out.printf("[Relational_Op, %s]", operator);
 	}
 }
